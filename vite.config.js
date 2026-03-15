65ab2df0f757c7435ec8eceebff599361541cae6
evanhwz <evan.haryo@ui.ac.id>
2026-03-15 19:43:53 +0700

    fix: add allowed host

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'node:process'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
   },
   preview: {
     host: '0.0.0.0',
-    port: 5173,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    allowedHosts: [
+      'frontend-mysawit-production.up.railway.app',
+      '.railway.app'
+    ]
   }
 })
