1c3b462b3784104b20c9eb6046702a1adb225324
evanhwz <evan.haryo@ui.ac.id>
2026-05-08 11:41:50 +0700

    feat: add route thorugh login and register

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './modules/auth/pages/Register';
+import Login from './modules/auth/pages/Login';
 import Users from './modules/auth/pages/Users';
 import KebunList from './modules/manajemen_kebun_sawit/pages/KebunList';
 import KebunForm from './modules/manajemen_kebun_sawit/pages/KebunForm';
@@ -9,7 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
         <Route path="/users" element={<Users />} />
         <Route path="/kebun" element={<KebunList />} />
         <Route path="/kebun/new" element={<KebunForm />} />
